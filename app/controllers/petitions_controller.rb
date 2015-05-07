2bff6117b6019d71f6f3aa85f2cd1f2567f49705
Shereef Bishay <shereef@gmail.com>
2015-05-06 23:40:06 -0700

    petitions rendering to json

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -1,4 +1,5 @@
 class PetitionsController < ApplicationController
   def index
+    render json: Petition.latest.limit(20)
   end
 end
--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,4 +1,8 @@
 class Petition < ActiveRecord::Base
     has_many :petition_users
     has_many :users, through: :petition_users
+    
+    def self.latest
+      order('published_at desc')
+    end
 end
